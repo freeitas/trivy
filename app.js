6f50be99539938bc7dc527f43619ab83e4c6c2cb
Guilherme Freitas <guilhermehenriquefreitasdasilv@gmail.com>
2025-03-07 14:00:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require("cookie-parser");
 const lodash = require("lodash"); // Package with a known vulnerability (CVE-2019-10744)
 const app = express();
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
